728c6bd117047a0c9565ce6017590d7af7989a99
sknafz <nafeesa@serpro.in>
2020-09-22 09:17:00 +0530

    Adding Test Done

--- PatientMedicalReport/src/testTemplates/EditTDController.java
+++ PatientMedicalReport/src/testTemplates/EditTDController.java
@@ -4,67 +4,93 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import DBConnection.DBConnectivity;
+import addTest.addTestController;
 import application.MainScreenController;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import screens.HomePageController;
 import viewPatient.ViewPDController;
 
 public class EditTDController {
 	@FXML
-    private AnchorPane pane_EditTD;
+	private AnchorPane pane_EditTD;
 
-    @FXML
-    private ScrollPane sEditPane;
+	@FXML
+	private ScrollPane sEditPane;
 
-    @FXML
-    private AnchorPane pane_EditTDContent;
+	@FXML
+	private AnchorPane pane_EditTDContent;
 
-    @FXML
-    private Label pGender;
+	@FXML
+	private Label pGender;
 
-    @FXML
-    private Label pAge;
+	@FXML
+	private Label pAge;
 
-    @FXML
-    private TextField refDoc;
+	@FXML
+	private TextField refDoc;
 
-    @FXML
-    private TextField testDate;
+	@FXML
+	private TextField testDate;
 
-    @FXML
-    private Label pId;
+	@FXML
+	private Label pId;
 
-    @FXML
-    private Label lblName;
-    
+	@FXML
+	private Label lblName;
+
 //    @FXML
 //    private VBox test_vbox;
 
-    @FXML
-    private Button btn_close;
+	@FXML
+	private Button btn_close;
 
-    public static ScrollPane s_editPane;
+	public static ScrollPane s_editPane;
+	public static Stage reportScreen;
+
 	public static AnchorPane paneEdit, testContentPane;
-	private static Label p_name,p_age,p_gender,p_id,test_name;
-	private static TextField ref_doctor,test_date;
+	private static Label p_name, p_age, p_gender, p_id, test_name;
+	private static TextField ref_doctor, test_date;
 	public static String tName;
 	private static Connection con;
 	private static PreparedStatement ps;
-	public static String past_history,menstural_data,clinical_impression,fetal_parameter,fetal_dop_studies,table1,table2,impression,note;
-	public static int tID, table1_col,table1_row,table2_col,table2_row;
-	public static VBox tvbox=new VBox(10);
-	public static Button btnSave=new Button("Save");
+	public static String past_history, menstural_data, clinical_impression, fetal_parameter, fetal_dop_studies, table1,
+			table2, impression, note;
+	public static int tID, table1_col, table1_row, table2_col, table2_row;
+	public static VBox tvbox = new VBox(10);
+	public static Button btnSave = new Button("Save");
+
+	private static int flag;
+
+	public static Stage getReportScreen() {
+		return reportScreen;
+	}
+
+	public static void setReportScreen(Stage reportScreen) {
+		addTestController.reportScreen = reportScreen;
+	}
+
 	public static String gettName() {
 		return tName;
 	}
@@ -72,7 +98,7 @@ public class EditTDController {
 	public static void settName(String tName) {
 		EditTDController.tName = tName;
 	}
-	
+
 	public static AnchorPane getTestContentPane() {
 		return testContentPane;
 	}
@@ -80,7 +106,7 @@ public class EditTDController {
 	public static void setTestContentPane(AnchorPane testContentPane) {
 		EditTDController.testContentPane = testContentPane;
 	}
-	
+
 	public static VBox getVbox() {
 		return tvbox;
 	}
@@ -88,29 +114,30 @@ public class EditTDController {
 	public static void setVbox(VBox tvbox) {
 		EditTDController.tvbox = tvbox;
 	}
-	
-    @FXML
+
+	@FXML
 	public void initialize() throws ClassNotFoundException, SQLException {
-		paneEdit=pane_EditTD;
-		s_editPane=sEditPane;
-		testContentPane=pane_EditTDContent;
-		p_age=pAge;
-		p_gender=pGender;
-		ref_doctor=refDoc;
-		p_id=pId;
-		p_name=lblName;
-		test_date=testDate;
-		
+		paneEdit = pane_EditTD;
+		s_editPane = sEditPane;
+		testContentPane = pane_EditTDContent;
+		p_age = pAge;
+		p_gender = pGender;
+		ref_doctor = refDoc;
+		p_id = pId;
+		p_name = lblName;
+		test_date = testDate;
+
 		String timeStamp = new SimpleDateFormat("dd/MM/yyyy").format(new Date());
 		test_date.setText(timeStamp);
 		patientData();
-    }
-    public static void patientData() {
+	}
+
+	public static void patientData() {
 		try {
 			con = DBConnectivity.getConnection();
-			int patientID=ViewPDController.getPID();
+			int patientID = ViewPDController.getPID();
 			ps = con.prepareStatement("SELECT * FROM patient_masterdata WHERE patient_id='" + patientID + "'");
-			
+
 			ResultSet rs = ps.executeQuery();
 			if (rs.next()) {
 				p_name.setText(rs.getString("patient_name"));
@@ -122,89 +149,187 @@ public class EditTDController {
 			e.printStackTrace();
 		}
 	}
-    
-    public static void screenContent(String testName) throws Exception {
-		Label lblTestname=new Label(testName);
+
+	public static void screenContent(String testName) throws Exception {
+		Label lblTestname = new Label(testName);
 		lblTestname.setLayoutX(210);
 		lblTestname.setLayoutY(75);
 		lblTestname.setPrefWidth(741);
 		lblTestname.setPrefHeight(32);
 		lblTestname.setTextAlignment(TextAlignment.CENTER);
-		
+
 		settName(testName);
 		tvbox.getChildren().clear();
 		testDetails();
-		
+
 		tvbox.setLayoutX(35);
 		tvbox.setLayoutY(110);
 		tvbox.setId("test_vbox");
 		tvbox.getChildren().add(btnSave);
 		btnSave.setPrefWidth(110);
 		btnSave.setPrefHeight(30);
-		btnSave.setStyle("-fx-font-size: 15; -fx-text-fill: white; -fx-background-color:  #2eacd2; -fx-padding: 2 2 2 2;");
+		btnSave.setStyle(
+				"-fx-font-size: 15; -fx-text-fill: white; -fx-background-color:  #2eacd2; -fx-padding: 2 2 2 2;");
 		System.out.println(getVbox());
-		testContentPane.getChildren().addAll(lblTestname,tvbox);
-    }
-    
-    public static void testDetails() throws Exception {
-		String testname=gettName();
+		testContentPane.getChildren().addAll(lblTestname, tvbox);
+	}
+
+	public static void testDetails() throws Exception {
+		String testname = gettName();
 		con = DBConnectivity.getConnection();
-		ps = con.prepareStatement("SELECT * FROM patient_report WHERE TEST_NAME='"+testname+"'");
+		ps = con.prepareStatement("SELECT * FROM patient_report WHERE TEST_NAME='" + testname + "'");
 		ResultSet rs = ps.executeQuery();
 		while (rs.next()) {
-			tID=rs.getInt("ID");
-			past_history=rs.getString("PAST HISTORY");
-			clinical_impression=rs.getString("CLINICAL_IMPRESSION");
-			fetal_parameter=rs.getString("FETAL PARAMETER");
-			fetal_dop_studies=rs.getString("FETAL DOPPLER STUDIES");
-			table1=rs.getString("TABLE1");
-			table1_col=rs.getInt("TABLE1_NO_OF_COLUMNS");
-			table1_row=rs.getInt("TABLE1_NO_OF_ROWS");
-			table2=rs.getString("TABLE2");
-			table2_col=rs.getInt("TABLE2_NO_OF_COLUMNS");
-			table2_row=rs.getInt("TABLE2_NO_OF_ROWS");  
-			impression=rs.getString("IMPRESSION");
-			note=rs.getString("PLEASE_NOTE");
+			tID = rs.getInt("ID");
+			past_history = rs.getString("PAST HISTORY");
+			clinical_impression = rs.getString("CLINICAL_IMPRESSION");
+			fetal_parameter = rs.getString("FETAL PARAMETER");
+			fetal_dop_studies = rs.getString("FETAL DOPPLER STUDIES");
+			table1 = rs.getString("TABLE1");
+			table1_col = rs.getInt("TABLE1_NO_OF_COLUMNS");
+			table1_row = rs.getInt("TABLE1_NO_OF_ROWS");
+			table2 = rs.getString("TABLE2");
+			table2_col = rs.getInt("TABLE2_NO_OF_COLUMNS");
+			table2_row = rs.getInt("TABLE2_NO_OF_ROWS");
+			impression = rs.getString("IMPRESSION");
+			note = rs.getString("PLEASE_NOTE");
 		}
 		TestContent.create_testDetails(tID);
-		if(past_history.equals("TRUE")) {
+		if (past_history.equals("TRUE")) {
 			System.out.println("past_history");
 			TestContent.create_pastHistory(tID);
 		}
-		if(clinical_impression.equals("TRUE")) {
+		if (clinical_impression.equals("TRUE")) {
 			TestContent.create_clinicalImp(tID);
 			System.out.println("clinical_impression");
 		}
-		if(fetal_parameter.equals("TRUE")) {
+		if (fetal_parameter.equals("TRUE")) {
 			System.out.println("fetal_parameter");
 			TestContent.create_fetalParameter(tID);
 		}
-		if(fetal_dop_studies.equals("TRUE")) {
+		if (fetal_dop_studies.equals("TRUE")) {
 			System.out.println("fetal_dop_studies");
 			TestContent.create_fetaldopStudies(tID);
 		}
-		if(table1.equals("TRUE")) {
+		if (table1.equals("TRUE")) {
 			System.out.println("table1");
 			TestContent.create_table1(tID);
 		}
-		if(table2.equals("TRUE")) {
+		if (table2.equals("TRUE")) {
 			System.out.println("table2");
 			TestContent.create_table2(tID);
 		}
-		if(impression.equals("TRUE")) {
+		if (impression.equals("TRUE")) {
 			System.out.println("impression");
 			TestContent.create_impression(tID);
 		}
-		if(note.equals("TRUE")) {
+		if (note.equals("TRUE")) {
 			System.out.println("note");
 			TestContent.create_note(tID);
 		}
+		btnSave.setOnAction(e -> {
+			try {
+				addTest();
+			} catch (Exception e1) {
+				Alert alert = new Alert(AlertType.WARNING);
+				alert.setTitle("Dr. Subodh App");
+				alert.setHeaderText(null);
+				alert.initStyle(StageStyle.TRANSPARENT);
+				alert.setContentText("Error1");
+				alert.showAndWait();
+				e1.printStackTrace();
+			}
+		});
 		ps.close();
 		rs.close();
 	}
 
+	public static void addTest() throws Exception {
+
+		String timeStamp = new SimpleDateFormat("dd.MM.yyyy.HH.mm.ss").format(new Date());
+		String insert = "INSERT into patient_reportmasterdata (regNumber,ref_doctor,testName,testDate,patientHistory,testDescription,clinicalImpression"
+				+ ",fetalParameter,fetalDoplerStudies,table1,table2,impression,note,active,created_timestamp,modified_timestamp)"
+				+ " values(?,?,?,?,?,?,?,?,?,?,?,?,?,'Y','" + timeStamp + "','" + timeStamp + "')";
+		con = DBConnectivity.getConnection();
+		try {
+			ps = con.prepareStatement(insert);
+			ps.setString(1, p_id.getText());
+			ps.setString(2, ref_doctor.getText());
+			ps.setString(3, gettName());
+			ps.setString(4, (test_date.getText()));
+			if (past_history.equals("TRUE")) {
+				ps.setString(5, TestContent.history.getHtmlText());
+			}else {
+				ps.setString(5, "");
+			}
+			ps.setString(6, TestContent.testDetstails.getHtmlText());
+			if (clinical_impression.equals("TRUE")) {
+				ps.setString(7, TestContent.clinicalImp.getHtmlText());
+			}else {
+				ps.setString(7, "");
+			}
+			if (fetal_parameter.equals("TRUE")) {
+				ps.setString(8, TestContent.fetalParameter.getHtmlText());
+			}else {
+				ps.setString(8, "");
+			}
+			if (fetal_dop_studies.equals("TRUE")) {
+				ps.setString(9, TestContent.fetaldopStudies.getHtmlText());
+			}else {
+				ps.setString(9, "");
+			}
+			if (table1.equals("TRUE")) {
+				ps.setString(10, TestContent.table1.getHtmlText());
+			}else {
+				ps.setString(10, "");
+			}
+			if (table2.equals("TRUE")) {
+				ps.setString(11, TestContent.table2.getHtmlText());
+			}else {
+				ps.setString(11, "");
+			}
+			if (impression.equals("TRUE")) {
+				ps.setString(12, TestContent.impression.getHtmlText());
+			}else {
+				ps.setString(12, "");
+			}
+			if (note.equals("TRUE")) {
+				ps.setString(13, TestContent.note.getHtmlText());
+			}else {
+				ps.setString(13, "");
+			}
+			flag = ps.executeUpdate();
+
+			if (flag > 0) { // redirecting to dashboard
+				Alert alert = new Alert(AlertType.WARNING);
+				alert.setTitle("Dr. Subodh App");
+				alert.setHeaderText(null);
+				alert.initStyle(StageStyle.TRANSPARENT);
+				alert.setContentText("Test Added Successfully");
+				alert.showAndWait();
+			} else {
+				Alert alert = new Alert(AlertType.WARNING);
+				alert.setTitle("Dr. Subodh App");
+				alert.setHeaderText(null);
+				alert.initStyle(StageStyle.TRANSPARENT);
+				alert.setContentText("Error2");
+				alert.showAndWait();
+			}
+
+		} catch (SQLException e) { // catching exception if any backend error occurs
+			Alert alert = new Alert(AlertType.WARNING);
+			alert.setTitle("Dr. Subodh App");
+			alert.setHeaderText(null);
+			alert.initStyle(StageStyle.TRANSPARENT);
+			alert.setContentText("Error while Inserting Data!! Please Try Again!");
+			alert.showAndWait();
+			e.printStackTrace();
+		}
+
+	}
+
 	@FXML
-    void cancelBtn(ActionEvent event) {
+	void cancelBtn(ActionEvent event) {
 		AnchorPane pane = MainScreenController.getHomePage();
 		for (int i = 0; i < pane.getChildren().size(); i++) {
 			String paneID = pane.getChildren().get(i).getId();
@@ -220,5 +345,5 @@ public class EditTDController {
 				break;
 			}
 		}
-    }
+	}
 }
--- PatientMedicalReport/src/testTemplates/TestContent.java
+++ PatientMedicalReport/src/testTemplates/TestContent.java
@@ -15,82 +15,108 @@ import javafx.scene.web.HTMLEditor;
 public class TestContent {
 	private static Connection con = DBConnectivity.getConnection();;
 	private static PreparedStatement ps;
-	private ResultSet rs;
-	public static AnchorPane contentPane=EditTDController.getTestContentPane();
-	public static VBox vbox=EditTDController.getVbox();
-	
+	public static AnchorPane contentPane = EditTDController.getTestContentPane();
+	public static VBox vbox = EditTDController.getVbox();
+	public static HTMLEditor history, clinicalImp, fetalParameter, fetaldopStudies, 
+	impression, note, table1, table2,testDetstails;
+
 	public static void create_testDetails(int tID) throws SQLException {
 		HTMLEditor he_Testdetails = new HTMLEditor();
 		he_Testdetails.setPrefHeight(370);
 		he_Testdetails.setPrefWidth(700);
 		he_Testdetails.setId("heTestdetails");
 		he_Testdetails.setStyle("-fx-border-color:white;");
-		
-		ps = con.prepareStatement("SELECT * FROM patient_report_testdetails WHERE tId='"+tID+"'");
+
+		ps = con.prepareStatement("SELECT * FROM patient_report_testdetails WHERE tId='" + tID + "'");
 		ResultSet rs = ps.executeQuery();
 		while (rs.next()) {
 			he_Testdetails.setHtmlText(rs.getString("testDetails"));
 		}
-		String text=PrintableData.stripHTMLTags(he_Testdetails.getHtmlText());
+		String text = PrintableData.stripHTMLTags(he_Testdetails.getHtmlText());
+
 		
-		if(!(text.equals(""))) {
+		if (!he_Testdetails.equals(null)) {
+			testDetstails = he_Testdetails;
+
+		} else {
+			testDetstails.setHtmlText("");		
+		}
+		if (!(text.equals(""))) {
 			vbox.getChildren().addAll(he_Testdetails);
 		}
 	}
-	
+
 	public static void create_pastHistory(int tID) throws SQLException {
-		Label lblphistory=new Label("Relevant past history");
+		Label lblphistory = new Label("Relevant past history");
 		HTMLEditor hE_pHistory = new HTMLEditor();
 		hE_pHistory.setPrefHeight(200);
 		hE_pHistory.setPrefWidth(700);
 		hE_pHistory.setId("hePastHistory");
 		hE_pHistory.setStyle("-fx-border-color:white;");
-		
-		ps = con.prepareStatement("SELECT * FROM patient_report_testdetails WHERE tId='"+tID+"'");
+
+		ps = con.prepareStatement("SELECT * FROM patient_report_testdetails WHERE tId='" + tID + "'");
 		ResultSet rs = ps.executeQuery();
 		while (rs.next()) {
 			hE_pHistory.setHtmlText(rs.getString("PAST HISTORY"));
 		}
-		
-		vbox.getChildren().addAll(lblphistory,hE_pHistory);
+		if (!hE_pHistory.equals(null)) {
+			history = hE_pHistory;
+
+		} else {
+
+			history.setHtmlText("");
+
+		}
+		vbox.getChildren().addAll(lblphistory, hE_pHistory);
 	}
-	
+
 	public static void create_clinicalImp(int tID) throws SQLException {
-		Label lblclinicalImp=new Label("CLINICAL IMPRESSION");
+		Label lblclinicalImp = new Label("CLINICAL IMPRESSION");
 		HTMLEditor hE_clinicalImp = new HTMLEditor();
 		hE_clinicalImp.setPrefHeight(370);
 		hE_clinicalImp.setPrefWidth(700);
 		hE_clinicalImp.setId("hePastHistory");
 		hE_clinicalImp.setStyle("-fx-border-color:white;");
-		
-		ps = con.prepareStatement("SELECT * FROM patient_report_testdetails WHERE tId='"+tID+"'");
+
+		ps = con.prepareStatement("SELECT * FROM patient_report_testdetails WHERE tId='" + tID + "'");
 		ResultSet rs = ps.executeQuery();
 		while (rs.next()) {
 			hE_clinicalImp.setHtmlText(rs.getString("CLINICAL_IMPRESSION"));
 		}
-		vbox.getChildren().addAll(lblclinicalImp,hE_clinicalImp);
+		if (!hE_clinicalImp.equals(null)) {
+			clinicalImp = hE_clinicalImp;
+		} else {
+			clinicalImp.setHtmlText("");
+		}
+		vbox.getChildren().addAll(lblclinicalImp, hE_clinicalImp);
 	}
-	
+
 	public static void create_fetalParameter(int tID) {
 		try {
-			Label lblfp=new Label("FETAL PARAMETERS: ");
-			
+			Label lblfp = new Label("FETAL PARAMETERS: ");
+
 			HTMLEditor hE_fp = new HTMLEditor();
 			hE_fp.setPrefHeight(200);
 			hE_fp.setPrefWidth(700);
 			hE_fp.setId("heFetalParameter");
 			hE_fp.setStyle("-fx-border-color:white;");
-			
-			ps = con.prepareStatement("SELECT * FROM patient_report_testdetails WHERE tId='"+tID+"'");
+
+			ps = con.prepareStatement("SELECT * FROM patient_report_testdetails WHERE tId='" + tID + "'");
 			ResultSet rs = ps.executeQuery();
 			while (rs.next()) {
 				hE_fp.setHtmlText(rs.getString("FETAL PARAMETER"));
 			}
-			vbox.getChildren().addAll(lblfp,hE_fp);
-		}catch (Exception e) {
+			if (!hE_fp.equals(null)) {
+				fetalParameter = hE_fp;
+			} else {
+				fetalParameter.setHtmlText("");
+			}
+			vbox.getChildren().addAll(lblfp, hE_fp);
+		} catch (Exception e) {
 			// TODO: handle exception
 		}
 	}
+
 	public static void create_fetaldopStudies(int tID) throws SQLException {
 		HTMLEditor hE_fds = new HTMLEditor();
 		hE_fds.setPrefHeight(200);
@@ -98,112 +124,122 @@ public class TestContent {
 		hE_fds.setId("heFetalDS");
 		hE_fds.setStyle("-fx-border-color:white;");
 
-		ps = con.prepareStatement("SELECT * FROM patient_report_testdetails WHERE tId='"+tID+"'");
+		ps = con.prepareStatement("SELECT * FROM patient_report_testdetails WHERE tId='" + tID + "'");
 		ResultSet rs = ps.executeQuery();
 		while (rs.next()) {
 			hE_fds.setHtmlText(rs.getString("FETAL DOPPLER STUDIES"));
 		}
+		if (!hE_fds.equals(null)) {
+			fetaldopStudies = hE_fds;
+		} else {
+			fetaldopStudies.setHtmlText("");
+		}
 		vbox.getChildren().addAll(hE_fds);
 	}
-	
+
 	public static void create_table1(int tID) throws SQLException {
-		HTMLEditor hE_table1= new HTMLEditor();
+		HTMLEditor hE_table1 = new HTMLEditor();
 		hE_table1.setPrefHeight(450);
 		hE_table1.setPrefWidth(700);
 		hE_table1.setId("hE_tbl1");
-		
+
 		// hide controls we don't need.
 		Node t_node = hE_table1.lookup(".top-toolbar");
-		Node b_node = hE_table1.lookup(".bottom-toolbar");		
+		Node b_node = hE_table1.lookup(".bottom-toolbar");
 		t_node.setVisible(false);
 		b_node.setVisible(false);
-		t_node.setStyle("-fx-max-width: 0px;\r\n" + 
-				"	-fx-min-width: 0px;\r\n" + 
-				"	-fx-pref-width: 0px;\r\n" + 
-				"	-fx-max-height: 0px;\r\n" + 
-				"	-fx-min-height: 0px;\r\n" + 
-				"	-fx-pref-height: 0px;\r\n" + 
-				"        -fx-opacity: 0;");
-		b_node.setStyle("-fx-max-width: 0px;\r\n" + 
-				"	-fx-min-width: 0px;\r\n" + 
-				"	-fx-pref-width: 0px;\r\n" + 
-				"	-fx-max-height: 0px;\r\n" + 
-				"	-fx-min-height: 0px;\r\n" + 
-				"	-fx-pref-height: 0px;\r\n" + 
-				"        -fx-opacity: 0;");
+		t_node.setStyle("-fx-max-width: 0px;\r\n" + "	-fx-min-width: 0px;\r\n" + "	-fx-pref-width: 0px;\r\n"
+				+ "	-fx-max-height: 0px;\r\n" + "	-fx-min-height: 0px;\r\n" + "	-fx-pref-height: 0px;\r\n"
+				+ "        -fx-opacity: 0;");
+		b_node.setStyle("-fx-max-width: 0px;\r\n" + "	-fx-min-width: 0px;\r\n" + "	-fx-pref-width: 0px;\r\n"
+				+ "	-fx-max-height: 0px;\r\n" + "	-fx-min-height: 0px;\r\n" + "	-fx-pref-height: 0px;\r\n"
+				+ "        -fx-opacity: 0;");
 		hE_table1.setStyle("-fx-border-color:white;");
 
-		ps = con.prepareStatement("SELECT * FROM patient_report_tabledetails WHERE testId='"+tID+"'");
+		ps = con.prepareStatement("SELECT * FROM patient_report_tabledetails WHERE testId='" + tID + "'");
 		ResultSet rs = ps.executeQuery();
 		while (rs.next()) {
 			hE_table1.setHtmlText(rs.getString("table"));
 		}
+		if (!hE_table1.equals(null)) {
+			table1 = hE_table1;
+		} else {
+			table1.setHtmlText("");
+		}
 		vbox.getChildren().addAll(hE_table1);
 	}
-	
+
 	public static void create_table2(int tID) throws SQLException {
-		HTMLEditor hE_table2= new HTMLEditor();
+		HTMLEditor hE_table2 = new HTMLEditor();
 		hE_table2.setPrefHeight(400);
 		hE_table2.setPrefWidth(700);
 		hE_table2.setId("hE_tbl2");
-		
+
 		// hide controls we don't need.
 		Node t_node = hE_table2.lookup(".top-toolbar");
-		Node b_node = hE_table2.lookup(".bottom-toolbar");	
+		Node b_node = hE_table2.lookup(".bottom-toolbar");
 		t_node.setVisible(false);
 		b_node.setVisible(false);
-		t_node.setStyle("-fx-max-width: 0px;\r\n" + 
-				"	-fx-min-width: 0px;\r\n" + 
-				"	-fx-pref-width: 0px;\r\n" + 
-				"	-fx-max-height: 0px;\r\n" + 
-				"	-fx-min-height: 0px;\r\n" + 
-				"	-fx-pref-height: 0px;\r\n" + 
-				"        -fx-opacity: 0;");
-		b_node.setStyle("-fx-max-width: 0px;\r\n" + 
-				"	-fx-min-width: 0px;\r\n" + 
-				"	-fx-pref-width: 0px;\r\n" + 
-				"	-fx-max-height: 0px;\r\n" + 
-				"	-fx-min-height: 0px;\r\n" + 
-				"	-fx-pref-height: 0px;\r\n" + 
-				"        -fx-opacity: 0;");
+		t_node.setStyle("-fx-max-width: 0px;\r\n" + "	-fx-min-width: 0px;\r\n" + "	-fx-pref-width: 0px;\r\n"
+				+ "	-fx-max-height: 0px;\r\n" + "	-fx-min-height: 0px;\r\n" + "	-fx-pref-height: 0px;\r\n"
+				+ "        -fx-opacity: 0;");
+		b_node.setStyle("-fx-max-width: 0px;\r\n" + "	-fx-min-width: 0px;\r\n" + "	-fx-pref-width: 0px;\r\n"
+				+ "	-fx-max-height: 0px;\r\n" + "	-fx-min-height: 0px;\r\n" + "	-fx-pref-height: 0px;\r\n"
+				+ "        -fx-opacity: 0;");
 		hE_table2.setStyle("-fx-border-color:white;");
 
-		ps = con.prepareStatement("SELECT * FROM patient_report_tabledetails WHERE testId='"+tID+"'");
+		ps = con.prepareStatement("SELECT * FROM patient_report_tabledetails WHERE testId='" + tID + "'");
 		ResultSet rs = ps.executeQuery();
 		while (rs.next()) {
 			hE_table2.setHtmlText(rs.getString("table"));
 		}
+		if (!hE_table2.equals(null)) {
+			table2 = hE_table2;
+		} else {
+			table2.setHtmlText("");
+		}
 		vbox.getChildren().addAll(hE_table2);
 	}
-	
+
 	public static void create_impression(int tID) throws SQLException {
-		Label lblimp=new Label("Impression: ");
+		Label lblimp = new Label("Impression: ");
 		HTMLEditor hE_imp = new HTMLEditor();
 		hE_imp.setPrefHeight(150);
 		hE_imp.setPrefWidth(700);
 		hE_imp.setId("heImpression");
 		hE_imp.setStyle("-fx-border-color:white;");
-		
-		ps = con.prepareStatement("SELECT IMPRESSION FROM patient_report_testdetails WHERE tId='"+tID+"'");
+
+		ps = con.prepareStatement("SELECT IMPRESSION FROM patient_report_testdetails WHERE tId='" + tID + "'");
 		ResultSet rs = ps.executeQuery();
 		while (rs.next()) {
 			hE_imp.setHtmlText(rs.getString("IMPRESSION"));
 		}
-		vbox.getChildren().addAll(lblimp,hE_imp);
+		if (!hE_imp.equals(null)) {
+			impression = hE_imp;
+		} else {
+			impression.setHtmlText("");
+		}
+		vbox.getChildren().addAll(lblimp, hE_imp);
 	}
-	public static void create_note(int tID) throws SQLException{
-		Label lblnote=new Label("Please Note: ");
+
+	public static void create_note(int tID) throws SQLException {
+		Label lblnote = new Label("Please Note: ");
 		HTMLEditor hE_note = new HTMLEditor();
 		hE_note.setPrefHeight(150);
 		hE_note.setPrefWidth(700);
 		hE_note.setId("heNote");
 		hE_note.setStyle("-fx-border-color:white;");
-		
-		ps = con.prepareStatement("SELECT PLEASE_NOTE FROM patient_report_testdetails WHERE tId='"+tID+"'");
+
+		ps = con.prepareStatement("SELECT PLEASE_NOTE FROM patient_report_testdetails WHERE tId='" + tID + "'");
 		ResultSet rs = ps.executeQuery();
 		while (rs.next()) {
 			hE_note.setHtmlText(rs.getString("PLEASE_NOTE"));
 		}
-		vbox.getChildren().addAll(lblnote,hE_note);
+		if (!hE_note.equals(null)) {
+			note = hE_note;
+		} else {
+			note.setHtmlText("");
+		}
+		vbox.getChildren().addAll(lblnote, hE_note);
 	}
 }
