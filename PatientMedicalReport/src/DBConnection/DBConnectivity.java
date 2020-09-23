694b18d26263a544273b1454e04f5b5509d037a1
sknafz <nafeesa@serpro.in>
2020-09-23 14:44:57 +0530

    pushing code

--- PatientMedicalReport/src/DBConnection/DBConnectivity.java
+++ PatientMedicalReport/src/DBConnection/DBConnectivity.java
@@ -8,7 +8,7 @@ public class DBConnectivity {
 		Connection con = null;
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/drsubodhapp","root","Babitha@123");
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/drsubodhapp2","root","root");
 //			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/drsubodhapp","root","root@123");
 //			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/drsubodhapp","root","shruthi14");
 			return con;
